36df74c4d48ba2ec2a1929019eef77bda55aa174
Eric <eric6356@gmail.com>
2015-05-13 13:08:50 +0800

    init model

--- blog/models.py
+++ blog/models.py
@@ -1,2 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.TextField()
+    body = models.TextField()
+    status = models.IntegerField(default=1)
+    gmt_create = models.DateTimeField(default=datetime.utcnow)
+    gmt_modify = models.DateTimeField(default=datetime.utcnow)
+    author = models.ForeignKey(User)
+    tags = models.ManyToManyField(Tag)
+    category = models.ForeignKey(Category)
+
+    def __str__(self):
+        return self.title
